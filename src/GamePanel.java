ab7cbab4981329180ad24f95b7d03232dc6b1064
MaehulMehra <68080910+MaehulMehra@users.noreply.github.com>
2021-09-02 17:06:04 -0700

    Create GamePanel.java

---
+++ src/GamePanel.java
@@ -0,0 +1,22 @@
+import javax.swing.JFrame;
+
+public class LeagueInvaders {
+	JFrame frame;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	GamePanel panel;
+	public static void main(String[] args) {
+		LeagueInvaders invaders = new LeagueInvaders ();
+		invaders.setup();
+	}
+	LeagueInvaders (){
+		frame = new JFrame ();
+		panel = new GamePanel ();
+	}
+	void setup () {
+		frame.add(panel);
+		frame.setVisible(true);
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
