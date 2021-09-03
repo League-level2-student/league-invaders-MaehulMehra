854a200dfbbce8c349a64d5deae5164cea90f8ad
MaehulMehra <68080910+MaehulMehra@users.noreply.github.com>
2021-09-02 17:05:26 -0700

    Update LeagueInvaders.java

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,21 @@
 
 public class LeagueInvaders {
-
+JFrame frame;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	GamePanel panel;
+	public static void main(String[] args) {
+		LeagueInvaders invaders = new LeagueInvaders ();
+		invaders.setup();
+	}
+	LeagueInvaders (){
+		frame = new JFrame ();
+		panel = new GamePanel ();
+	}
+	void setup () {
+		frame.add(panel);
+		frame.setVisible(true);
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
